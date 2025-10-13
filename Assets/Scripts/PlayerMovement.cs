2151d62ff4464763441c65d32cdd4be19ded3d17
T301104 <geurts991@gmail.com>
2025-10-13 13:32:17 +0200

    made worst movement script ever

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [SerializeField] private float jumpStrength = 10;
     [SerializeField] private float speed = 10;
     InputAction playerMoveAction;
+    InputAction jumpAction;
+
 
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,11 +16,16 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody>();
         playerMoveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (jumpAction.IsPressed())
+        {
+            rb.AddForce(Vector3.up * jumpStrength);
+        }
         Vector2 playerMovement = new Vector2(playerMoveAction.ReadValue<Vector2>().x, 0) * speed;
         rb.AddForce(playerMovement); 
         
