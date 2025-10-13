621177afcee00d8636b9a83be4995e5906351b03
T301104 <geurts991@gmail.com>
2025-10-13 13:18:43 +0200

    Added unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10;
+    InputAction playerMoveAction;
+
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        playerMoveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 playerMovement = new Vector2(playerMoveAction.ReadValue<Vector2>().x, 0) * speed;
+        rb.AddForce(playerMovement); 
+        
+    }
+}
