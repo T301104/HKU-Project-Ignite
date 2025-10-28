b4f213725cca90034ac734a0c667265d16885a3a
T301104 <geurts991@gmail.com>
2025-10-28 14:52:14 +0100

    kind of fixed player movement (check if ok)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float jumpStrength = 10;
     [SerializeField] private float speed = 10;
+    [SerializeField] private float friction = 0.4f;
+    Collider coll;
+    bool frictionZero = false;
     InputAction playerMoveAction;
     InputAction jumpAction;
     NpcMonkey npcMonkey;
@@ -18,17 +21,28 @@ public class PlayerMovement : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
         playerMoveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        coll = GetComponent<Collider>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (jumpAction.IsPressed())
+        if (jumpAction.WasPressedThisFrame())
         {
             rb.AddForce(Vector3.up * jumpStrength);
         }
-        Vector2 playerMovement = new Vector2(playerMoveAction.ReadValue<Vector2>().x, 0) * speed;
+        Vector2 playerMovement = new Vector2(playerMoveAction.ReadValue<Vector2>().x, 0) * speed *Time.deltaTime;
         rb.AddForce(playerMovement);
+
+        if (playerMovement != Vector2.zero)
+        {
+            frictionZero = true;
+            coll.material.dynamicFriction = 0;
+        }
+        else
+        {
+            coll.material.dynamicFriction = friction;
+        }
 
     }
     
